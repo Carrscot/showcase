6bab11f6695539f7f2ed2985a15255f96bca6407
Scotty <carrscot@gmail.com>
2022-11-24 14:46:43 +0200

    Add event listeners in script.js to change class of titleBox div. Add links to event listeners for respective pages in script.js

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ MAIN PAGE:
 	Large centered boxes which contain the title of the project. Below each box will be a very brief description of the project.
 		>>>Each box will be coloured the same as the project page and the border will be "highlighted" and expand upon mouseover.
 		>>>Upon clicking the box the page will changed into the linked project.
+
+
+Git commit when online "Add event listeners in script.js to change class of titleBox div. Add links to event listeners for respective pages in script.js"
+
+offline changes:
+
+EAS - background style, spelling errors
+
+RPS - CSS changes
+
+REcipes - CSS changes
--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const change = document.querySelectorAll('div');
-document.getElementsByClassName('titleBox').addEventListener('mouseover', () => {console.log('fuck you')});
+const change = document.querySelectorAll('.titleBox');
+    change.forEach(div => div.addEventListener('mouseover', () => {div.classList.add('mouseOver')}));
+    change.forEach(div => div.addEventListener('mouseout', () => {div.classList.remove('mouseOver')}));
+    
+const calc = document.querySelector('#calc');
+    calc.addEventListener('click',() => window.open('Calculator/index.html', '_blank'));
+
+const EAS = document.querySelector('#EAS');
+    EAS.addEventListener('click',() => window.open('EAS/index.html', '_blank'));
+
+const RPS = document.querySelector('#RPS');
+    RPS.addEventListener('click',() => window.open('rock-paper-scissors/index.html', '_blank'));
+
+const landing = document.querySelector('#landing');
+    landing.addEventListener('click',() => window.open('landing-page/index.html', '_blank'));
+
+const recipes = document.querySelector('#recipes');
+    recipes.addEventListener('click',() => window.open('odin_recipes/index.html', '_blank'));
